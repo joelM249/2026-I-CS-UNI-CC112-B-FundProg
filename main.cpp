5f61029e462d45a20087151807fa3ffa614709a9
joelM249 <annelids4x4@gmail.com>
2026-04-01 14:46:00 -0500

    primer commit

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 
 // g++ -std=c++2b main.cpp -o main
 // El main() debe ser muy pero muy pequeñito
+// .\main 
 int main() {
     std::cout << "Hello FundProg-UNI!" << std::endl;
 
-    return 0;
-}
+    return 0;}
